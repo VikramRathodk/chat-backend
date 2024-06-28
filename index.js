4825b51d6e3f40784f79c8c89b4d3102c3754614
rathodvikramk382@gmail.com <rathodvikramk382@gmail.com>
2024-06-28 21:59:46 +0530

    allowed url encoded data in params

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 const startServer = async () => {
   try {
