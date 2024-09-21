f2333daacfe60e5b1c38a3358ddd9e8ba77d49bf
rathodvikramk382@gmail.com <rathodvikramk382@gmail.com>
2024-09-21 11:10:25 +0530

    123

--- config/firebase/firebase_config.js
+++ config/firebase/firebase_config.js
@@ -1,13 +1,10 @@
-import admin from "firebase-admin";
-import { readFileSync } from "fs";
-import { join } from "path";
-
-const serviceAccountPath = join(__dirname, "service_account.json");
-const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, "utf-8"));
+import admin from 'firebase-admin';
+import serviceAccount from './service_account.json' assert { type: 'json' };
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 export const db = admin.firestore();
+
 export default admin;
